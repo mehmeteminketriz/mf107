432d81c87d482299e7e14bef1f2acb406fef5063
Mehmet Emin KETRİZ <89727826+mehmeteminketriz@users.noreply.github.com>
2025-07-07 21:59:18 +0300

    son

---
+++ hafta1/gun1/JSORnek.js
@@ -0,0 +1,20 @@
+/**
+ * int sayi = 5;
+ * sayi = 7;,
+ * sayi = "Serkan" izin vermeez
+ */
+
+/**
+ * javascript de veri tipleri dinamik olarak değişebilir. aşağıdaki gibi. c# ta öyle değil baştan belirlemen gerekir
+ * ve javascript c#a göre daha esnek bir yapıya sahip olsa da performans açısından c# a göre daha yavaştır.
+ * let
+ * const
+ * var
+ */
+
+let sayi = 5;
+console.log(sayi);
+sayi = "Mehmet"
+console.log(sayi);
+let ad = "Ahmet";
+let aktif = true
